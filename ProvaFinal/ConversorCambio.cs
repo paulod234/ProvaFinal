ea8aad7b20532619e8e9f121e52506a284b02444
Paulo Ricardo Nascimento Lins <pauloricardo_nl@hotmail.com>
2019-12-11 21:27:48 -0300

    Add project files.

---
+++ ProvaFinal/ConversorCambio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+namespace ProvaFinal
+{
+    class ConversorCambio
+    {
+        static object Main(string[] args)
+        {
+
+            double precoReal = 0;
+            double taxa = 4.250;
+            double conversao = 0;
+
+            Console.Write("Entre com o preço em Real: ");
+            precoReal = double.Parse(Console.ReadLine());
+
+            conversao = precoReal / taxa;
+
+            Console.Write("Preço convertido em dolar ! $" + conversao + "Dolares");
+            Console.ReadKey();
+        }
+
+
+    }
+}
